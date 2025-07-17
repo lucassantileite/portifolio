14d4279208c6f252b6c2f78c5808da4e04864eb5
lucassantileite <lucassanti.tinfem2023@gmail.com>
2025-07-17 00:34:44 -0300

    deixando tudo mais claro

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,19 +1,11 @@
 $(document).ready(function(){
-    $(window).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        var headerToDisplay = false;
-        $('.header[data-visible-range]').each(function() {
-            var range = $(this).data('visible-range').split('-');
-            range[0] = range[0] ? parseInt(range[0], 10) : 0;
-            range[1] = range[1] ? parseInt(range[1], 10) : $(document).height();
-            if(scrollTop >= range[0] && scrollTop <= range[1]) {
-                headerToDisplay = $(this);
-                return false;
-            }
-        });
-        if(headerToDisplay && !headerToDisplay.is(':visible')) {
-            $('.header[data-visible-range]').hide();
-            headerToDisplay.show();
-        }
-    });
+    /*fixed header invades section */
+    $('.img-phrase').css({height: window.innerHeight});
+    $(window).scroll(function(){
+        var header = $('.header');
+        if($(window).scrollTop() > 0){
+            header.css('background-color', 'rgba(255, 255, 255, 1)');
+            $('.header *').css('color', 'black');
+        }else header.css('background-color', 'unset') && $('.header *').css('color', 'white');
+    })
 });
