734cbc61d385382cb2e5759f3b069624b89376f3
lucassantileite <lucassanti.tinfem2023@gmail.com>
2025-07-18 14:47:51 -0300

    responsividade total

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -5,6 +5,28 @@ $(document).ready(function(){
         if($(window).scrollTop() > 0){
             header.css('background-color', 'rgba(255, 255, 255, 1)');
             $('.header *').css('color', 'black');
-        }else header.css('background-color', 'unset') && $('.header *').css('color', 'white');
+        }else 
+            {
+                header.css('background-color', 'unset');
+                $('.header h1 a').css('color', 'white');
+                $('.header .nav a').css('color', 'white');
+            }
     })
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }
 });
+
+function dropdown(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
